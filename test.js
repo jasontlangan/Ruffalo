d42a03181a259541a5fec671bface199080d70e1
Thomas Langan <tom.langan@comcast.net>
2022-03-22 14:14:54 -0400

    Initial Commit

---
+++ test.js
@@ -0,0 +1,10 @@
+function addToList() {
+    document.getElementById("image").style.display="inline"
+}
+
+var a = [1, 2, 3]
+var s = a.map(function f(a) { return a }).reverse()
+
+
+var s = 1;
+console.log(s)
